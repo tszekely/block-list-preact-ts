80205df7876e9e3b43aa8d584e5ec085ba5078f7
Timotheus Szekely <timotheus.szekely@gmail.com>
2021-04-02 15:45:26 +0300

    Add URL param page size modifier (ex: ?pageSize=10).

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,9 +4,14 @@ import BlockList from "./BlockList";
 import style from './App.module.css';
 import {useCallback, useState} from "preact/hooks";
 
+const urlParams = new URLSearchParams(window.location.search);
+const urlPageSize = parseInt(urlParams.get("pageSize") || '');
+
 const App: FunctionalComponent = () => {
+  let initialPageSize = isNaN(urlPageSize) ? 10 : urlPageSize;
+
   const [page, setPage] = useState(1);
-  const [pageSize, setPageSize] = useState(100);
+  const [pageSize, setPageSize] = useState(initialPageSize);
 
   const onPrevPage = useCallback(() => setPage(page => page - 1), [setPage]);
   const onNextPage = useCallback(() => setPage(page => page + 1), [setPage]);
@@ -19,11 +24,11 @@ const App: FunctionalComponent = () => {
 
         <label className={style.items}>
           <span>Items: </span>
-          <input type="number" value={pageSize} onKeyUp={handlePageSizeChange} />
+          <input type="number" value={pageSize} onKeyUp={handlePageSizeChange}/>
         </label>
       </header>
 
-      <BlockList page={page} pageSize={pageSize} />
+      <BlockList page={page} pageSize={pageSize}/>
 
       <footer>
         <div class="paging">
